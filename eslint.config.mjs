f826d9ff5675aa2eae5f8645e373c2d37af5ebe6
Sai Xiong <saixiong@gmail.com>
2026-09-14 13:39:09 -0500

    chore: lint ignores out/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist/**', '.vscode-test/**', 'node_modules/**', '*.vsix'] },
+  { ignores: ['dist/**', 'out/**', '.vscode-test/**', 'node_modules/**', '*.vsix'] },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
